cf8ce92061955f991f0c67f0637260b2f5f99078
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2017-10-12 15:04:20 -0400

    phone fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parking.java
@@ -1,32 +1,34 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robot.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robot.hardware.ColorSensor;
-/**
- * Created by dvw24 on 10/10/17.
- */
-public class Parking extends OpMode {
-
-    private DcMotor rightmotor, leftmotor;
-    private ColorSensor zoneSensor;
-
-    @Override
-    public void init() {
-        rightmotor = hardwareMap.DcMotor.get("m1");
-        leftmotor = hardwareMap.DcMotor.get("m2");
-
-        telemetry.addData("Stuff!", "Motors and Sensors ready!");
-
-        while(true){
-            rightmotor.setPower(1);
-            leftmotor.setPower(1);
-
-            if(zoneSensor = zoneSensor.white){
-                break;
-            }
-        }
-    }
-
-
-
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//
+///**
+// * Created by dvw24 on 10/10/17.
+// */
+//public class Auto_Move extends OpMode {
+//
+//    private DcMotor leftmotor, rightmotor;
+//    private double wheelSpeed;
+//
+//    @Override
+//    public void init() {
+//        rightmotor = hardwareMap.dcMotor.get("m2");
+//        leftmotor = hardwareMap.dcMotor.get("m1");
+//
+//        while(true){
+//            rightmotor.setPower(1);
+//            leftmotor.setPower(1);
+//
+////            if(System ==true){
+////                break;
+////
+////            }
+//        }
+//
+//
+//        //telemetry.addData("Message 1", "Motors and servos declared! All Systems go!");
+//    }
+//
+//}
