b1d664e77ece2a28d1c2e23ec45f5d63842d7e73
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2017-10-17 19:35:23 -0400

    fixed lance's mistake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Balls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Balls.java
@@ -1,49 +1,56 @@
-//package org.firstinspires.ftc.teamcode;
-//
-///**
-// * Created by dvw24 on 10/10/17.
-// */
-//
-//import com.qualcomm.robotcore.hardware.Servo;
-//import com.qualcomm.robotcore.hardware.ColorSensor;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//
-//
-//public class Color_Balls extends OpMode {
-//
-//    private Servo colorKnocker;
-//    private ColorSensor jeweler;
-//    private DcMotor leftmotor, rightmotor;
-//
-//    @Override
-//    public void init() {
-//        colorKnocker = hardwareMap.Servo.get("s1");
-//        jeweler = hardwareMap.ColorSensor.get("c1");
-//        leftmotor = hardwareMap.dcMotor.get("m1");
-//        rightmotor = hardwareMap.dcMotor.get("m2");
-//
-//        telemetry.addData("Stuff!", "Servos, Motors and Sensors declared! All Systems go!");
-//
-//        while(true){
-//            colorKnocker.setPostion(1);
-//
-//            if(jeweler= jeweler.blue){
-//                rightmotor.setPower(1);
-//                leftmotor.setPower(1);
-//
-//                if(System.currentTimeMillis(4000)){
-//                    break;
-//                    colorKnocker.setPosition(0);
-//                }
-//            }
-//        }
-//
-//
-//
-//    }
-//
-//
-//}
-//
-//
+package org.firstinspires.ftc.teamcode;
+
+/**
+* Created by dvw24 on 10/10/17.
+*/
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
+
+
+public class Color_Balls extends LinearOpMode {
+
+   private Servo colorKnocker;
+   private ColorSensor color;
+   private DcMotor leftmotor, rightmotor;
+
+   @Override
+   public synchronized void waitForStart() {
+       super.waitForStart();
+   }
+
+
+   public void runOpMode() {
+       colorKnocker = hardwareMap.servo.get("s1");
+       color = hardwareMap.colorSensor.get("color");
+       leftmotor = hardwareMap.dcMotor.get("m1");
+       rightmotor = hardwareMap.dcMotor.get("m2");
+
+       telemetry.addData("Hi!", "Servos, Motors and Sensors declared! All Systems go!");
+
+       color.enableLed(true);
+
+       if (color.red() < 255 && color.red() > 100) {
+           rightmotor.setPower(1);
+           leftmotor.setPower(1);
+       }else{
+           rightmotor.setPower(-1);
+           leftmotor.setPower(-1);
+       }
+
+
+
+   }
+
+
+}
+
+
+
+
