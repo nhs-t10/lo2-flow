ed3edb9587845f5d727afa7bf2e5c9b246bc5f68
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-10-18 19:14:50 -0400

    this is the gyro program i guess, do stuff? idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,97 +8,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Created by Alex on 10/17/2017.
  */
 
-public class Gyro extends GyroSensor {
-    public GyroSensor gyro = new GyroSensor() {
+public class Gyro extends OpMode {
 
 
-        @Override
-        public void calibrate() {
+    private GyroSensor gyro;
 
-        }
-        int y = rawY();
-        int x = rawX();
-        int z = rawZ();
-        if( y > 0){
-            if ( x > 0){
-//if Y is changing, then we know platform is moving
+
+    @Override
+    public void init() {
+        gyro = hardwareMap.gyroSensor.get("???"); //TODO: Find real name!!!
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        int y = gyro.rawY();
+        int x = gyro.rawX();
+        int z = gyro.rawZ();
+        if( y > 0) {
+            if ( x > 0) {
+                //if Y is changing, then we know platform is moving
             }
-            if ( z > 0){
-//then we need to figure out which direction the robot is moving
- // we will move the opposite of that direction
- // need to figure out how to turn
+            if ( z > 0) {
+                //then we need to figure out which direction the robot is moving
+                // we will move the opposite of that direction
+                // need to figure out how to turn
             }
         }
-
-
-        @Override
-        public boolean isCalibrating() {
-            return false;
-        }
-
-        @Override
-        public int getHeading() {
-            return 0;
-        }
-
-        @Override
-        public double getRotationFraction() {
-            return 0;
-        }
-
-        @Override
-        public int rawX() {
-            return 0;
-        }
-
-        @Override
-        public int rawY() {
-            return 0;
-        }
-
-        @Override
-        public int rawZ() {
-            return 0;
-        }
-
-        @Override
-        public void resetZAxisIntegrator() {
-
-        }
-
-        @Override
-        public String status() {
-            return null;
-        }
-
-        @Override
-        public Manufacturer getManufacturer() {
-            return null;
-        }
-
-        @Override
-        public String getDeviceName() {
-            return null;
-        }
-
-        @Override
-        public String getConnectionInfo() {
-            return null;
-        }
-
-        @Override
-        public int getVersion() {
-            return 0;
-        }
-
-        @Override
-        public void resetDeviceConfigurationForOpMode() {
-
-        }
-
-        @Override
-        public void close() {
-
-        }
-    };
+    }
 }
