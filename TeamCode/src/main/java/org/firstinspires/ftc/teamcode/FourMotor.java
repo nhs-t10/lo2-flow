a159254a8fc9152e9f17f91e0b75f4b5ed4b35b0
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2017-10-19 15:21:33 -0400

    major movement changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourMotor.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by tripszewczak on 10/5/17.
  */
@@ -19,56 +21,95 @@ public class FourMotor extends OpMode {
         lf = hardwareMap.dcMotor.get("m1");
         rb = hardwareMap.dcMotor.get("m4");
         lb = hardwareMap.dcMotor.get("m3");
-        rf.setPower(0);
-        lf.setPower(0);
-        rb.setPower(0);
-        lb.setPower(0);
-
 
         telemetry.addData("Message 1", "Motors and Servos Declared! All Systems go!");
-
-
-
     }
 
+    @Override
     public void loop(){
+        double forward = gamepad1.left_stick_y;
+        double turning = gamepad1.left_stick_x;
 
-        if (gamepad1.left_stick_y>0.25){
+        forward = Range.clip(forward,-1,1);
+        turning = Range.clip(turning,-1,1);
 
-            lf.setPower(1);
-            rf.setPower(1);
-            rb.setPower(-1);
-            lb.setPower(-1);
+
+        forward = (double)scaleInput(forward);
+        turning = (double)scaleInput(turning);
+
+        if (forward>0.25){
+
+            lf.setPower(forward);
+            rf.setPower(forward);
+            rb.setPower(-forward);
+            lb.setPower(-forward);
         }
 
-        if (gamepad1.left_stick_y<0.25){
-            lf.setPower(1);
-            rf.setPower(1);
-            rb.setPower(-1);
-            lb.setPower(-1);
+        if (forward<-0.25){
+
+            lf.setPower(forward);
+            rf.setPower(forward);
+            rb.setPower(-forward);
+            lb.setPower(-forward);
 
         }
 
-        if (gamepad1.left_stick_x>0.25){
-            lb.setPower(-1);
-            lf.setPower(1);
-            rf.setPower(-1);
-            rb.setPower(1);
+        if (turning>0.25){
+
+            lb.setPower(-turning);
+            lf.setPower(turning);
+            rf.setPower(-turning);
+            rb.setPower(turning);
 
         }
-        if (gamepad1.left_stick_x<0.25){
-            lb.setPower(1);
-            lf.setPower(-1);
-            rf.setPower(1);
-            rb.setPower(-1);
+        if (turning<-0.25) {
+
+            lb.setPower(turning);
+            lf.setPower(-turning);
+            rf.setPower(turning);
+            rb.setPower(-turning);
         }
 
 
 
-        telemetry.addData("Left motor values",gamepad1.left_stick_y);
-        telemetry.addData("Right motor values",gamepad1.right_stick_y);
-
+        telemetry.addData("Left motor values",forward);
+        telemetry.addData("Right motor values",turning);
 
     }
+
+    @Override
+    public void stop(){
+
+    }
+
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.    
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        // James Says: This code works just fine, except you could also use "scaleArray.length"
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+        // return scaled value.
+        return dScale;
+    }
+
 
 }
