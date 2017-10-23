d2f43846efe2f6f83f14f70a21db907a2ac791c2
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-10-23 09:14:13 -0400

    this is the gyro program i guess, do stuff? idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by Alex on 10/17/2017.
@@ -14,20 +15,19 @@ public class Gyro extends OpMode {
 
 
     private GyroSensor gyro;
-    private DcMotor leftmotor, rightmotor, leftmotor2, rightmotor2;
+    private DcMotor lf, rf, lb, rb;
 
     @Override
     public void init() {
         gyro = hardwareMap.gyroSensor.get("Gyro1");
-        rightmotor = hardwareMap.dcMotor.get("m2");
-        leftmotor = hardwareMap.dcMotor.get("m1");
-        leftmotor2 = hardwareMap.dcMotor.get("m3");
-        rightmotor2 = hardwareMap.dcMotor.get("m4");
+        rf = hardwareMap.dcMotor.get("m1");
+        lf = hardwareMap.dcMotor.get("m0");
+        lb = hardwareMap.dcMotor.get("m2");
+        rb = hardwareMap.dcMotor.get("m3");
     }
 
     @Override
     public void loop() {
-
         int y = gyro.rawY();
         int x = gyro.rawX();
         int z = gyro.rawZ();
@@ -50,15 +50,15 @@ public class Gyro extends OpMode {
 
                 }
             } else if (z < 0) {
-                leftmotor.setPower(1);
-                leftmotor2.setPower(1);
-                rightmotor.setPower(1);
-                rightmotor2.setPower(1);
+                lf.setPower(1);
+                lb.setPower(1);
+                rf.setPower(1);
+                rb.setPower(1);
             } else if (z > 0) {
-                leftmotor.setPower(-1);
-                leftmotor2.setPower(-1);
-                rightmotor.setPower(-1);
-                rightmotor2.setPower(-1);
+                lf.setPower(-1);
+                lb.setPower(-1);
+                rf.setPower(-1);
+                rb.setPower(-1);
             }
         }
     }
