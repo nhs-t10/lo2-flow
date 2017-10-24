24579cde6f101bbfd47653e108fc435e4e40cc4d
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-10-24 19:39:47 -0400

    this is the gyro program i guess, do stuff? idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -13,41 +13,71 @@ import com.qualcomm.robotcore.util.Range;
 public class Gyro extends OpMode {
 
 
-
     private GyroSensor gyro;
     private DcMotor lf, rf, lb, rb;
+
+
 
     @Override
     public void init() {
         gyro = hardwareMap.gyroSensor.get("Gyro1");
-        rf = hardwareMap.dcMotor.get("m1");
         lf = hardwareMap.dcMotor.get("m0");
+        rf = hardwareMap.dcMotor.get("m1");
         lb = hardwareMap.dcMotor.get("m2");
         rb = hardwareMap.dcMotor.get("m3");
     }
 
+    public void setGyro(GyroSensor gyro) {
+        this.gyro = gyro;
+        gyro.calibrate();
+    }
+
     @Override
-    public void loop() {
+    public double getRuntime() {
+        return super.getRuntime();
+    }
+
+    @Override
+    while (timeCheck = 0; timeCheck < 30; ) {
         int y = gyro.rawY();
         int x = gyro.rawX();
         int z = gyro.rawZ();
         if (y < 0) {
             if (x > 0) {
                 if (z > 0) {
+                    lf.setPower(1);
+                    lb.setPower(0);
+                    rf.setPower(0);
+                    rb.setPower(1);
 
                 } else if (z < 0) {
-
+                    lf.setPower(0);
+                    lb.setPower(-1);
+                    rf.setPower(-1);
+                    rb.setPower(0);
                 } else {
+                    lf.setPower(-1);
+                    lb.setPower(1);
+                    rf.setPower(-1);
+                    rb.setPower(1);
 
                 }
             } else if (x < 0) {
                 if (z < 0) {
-
-
+                    lf.setPower(-1);
+                    lb.setPower(0);
+                    rf.setPower(0);
+                    rb.setPower(-1);
                 } else if (z > 0) {
-
+                    lf.setPower(0);
+                    lb.setPower(1);
+                    rf.setPower(1);
+                    rb.setPower(0);
                 } else {
-
+                    lf.setPower(1);
+                    lb.setPower(-1);
+                    rf.setPower(1);
+                    rb.setPower(-1);
                 }
             } else if (z < 0) {
                 lf.setPower(1);
