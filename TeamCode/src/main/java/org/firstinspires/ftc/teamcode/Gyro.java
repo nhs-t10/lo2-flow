23d1d8e1afee0e14ae303044d95b058fbe1b6709
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-11-02 15:40:51 -0400

    this is the gyro program i guess, do stuff? idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -22,19 +22,16 @@ public class Gyro extends OpMode {
         rf = hardwareMap.dcMotor.get("m1");
         lb = hardwareMap.dcMotor.get("m2");
         rb = hardwareMap.dcMotor.get("m3");
-
     }
     private void wheelSet(double a0, double a1, double a2, double a3){
         lf.setPower(a0);
         lb.setPower(a1);
         rf.setPower(a2);
         rb.setPower(a3);
-
     }
-    private void resetGyro(){
+    private void resetGyro() {
         gyro.calibrate();
     }
-
     @Override
     public double getRuntime() {
         return super.getRuntime();
@@ -50,12 +47,9 @@ public class Gyro extends OpMode {
                     resetGyro();
                 }
                 if (y < 0) {
-
-
                     if (x > 0) {
                         if (z > 0) {
                             wheelSet(1, 0, 0, 1);
-
                         } else if (z < 0) {
                             wheelSet(0, -1, -1, 0);
                         } else {
