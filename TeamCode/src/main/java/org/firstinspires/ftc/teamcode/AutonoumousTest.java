1d7780e750bf46678fe960870cde18bdf579442a
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-11-20 10:31:27 -0500

    autonomous (feat yoda) movement this is

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonoumousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonoumousTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -13,29 +16,41 @@ import com.qualcomm.robotcore.util.Range;
 
 @Autonomous(name = "autonomous movement text")
 
-public class AutonoumousTest extends OpMode {
-    AutonoumousTest hecker = new AutonoumousTest;
+public class AutonoumousTest extends LinearOpMode {
+    AutonoumousTest hecker = new AutonoumousTest();
 
-    DcMotor lf,lb,rf,rb;
-
-    public void init() {
-        lf = hardwareMap.dcMotor.get("m0");
-        lb = hardwareMap.dcMotor.get("m1");
-        rf = hardwareMap.dcMotor.get("m2");
-        rb = hardwareMap.dcMotor.get("m3");
+    DcMotor lf, lb, rf, rb;
 
 
+
+    @Override public synchronized void waitForStart(){
+        super.waitForStart();
     }
+
 
     private void wheelSet(double a0, double a1, double a2, double a3) {
         lf.setPower(a0);
         lb.setPower(a1);
         rf.setPower(a2);
         rb.setPower(a3);
+    }
+     public void main(){
+        lf = hardwareMap.dcMotor.get("m0");
+        lb = hardwareMap.dcMotor.get("m1");
+        rf = hardwareMap.dcMotor.get("m2");
+        rb = hardwareMap.dcMotor.get("m3");
+        //lf.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        //lb.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        //rf.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        //rb.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        lb.setDirection(DcMotor.Direction.REVERSE);
 
     }
+    hecker.waitForStart();
 
 
-    }
+
+
 
 }
