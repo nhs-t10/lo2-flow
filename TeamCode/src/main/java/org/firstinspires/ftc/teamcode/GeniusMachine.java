373ca2361ac428a9c421dbada611e9be5c225900
Lancey Pancey <zlancelot117@gmail.com>
2017-11-21 16:12:01 -0500

    genius Machine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GeniusMachine.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by dvw25 on 11/21/17.
+ */
+
+public class GeniusMachine extends OpMode{
+    private DcMotor GM;
+
+    @Override
+    public void init() {
+        GM = hardwareMap.dcMotor.get("g1");
+
+        telemetry.addData("Message 1", "Servos declared! All Systems go!");
+    }
+    @Override
+    public void loop(){
+        if(gamepad1.right_trigger > 0){
+            GM.setPower(1);
+        }if(gamepad1.left_trigger > 0){
+            GM.setPower(-1);
+        }else{
+            GM.setPower(0);
+        }
+
+
+
+    }
+}
