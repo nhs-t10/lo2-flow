bcb56f716ae7fa6243260afceb2390a2136a4c4e
Lancey Pancey <zlancelot117@gmail.com>
2017-11-21 15:23:31 -0500

    servo for glyphs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clamp.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+/**
+ * Created by dvw25 on 11/21/17.
+ */
+
+public class Clamp extends OpMode {
+    private Servo squeeze;
+    double x = 0;
+
+    @Override
+    public void init() {
+        squeeze = hardwareMap.servo.get("s1");
+
+
+        telemetry.addData("Message 1", "Servos declared! All Systems go!");
+    }
+    @Override
+    public void loop(){
+
+
+        if(gamepad1.right_stick_y > 0){
+            x = x + .1;
+            squeeze.setPosition(x);
+        }
+        if(gamepad1.right_stick_y < 0){
+            x = x - .1;
+            squeeze.setPosition(x);
+        }
+
+    }
+
+}
