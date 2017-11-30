f8c2f9d924f4f7fa3088f74948ee3560965501bb
Lancey Pancey <zlancelot117@gmail.com>
2017-11-30 15:54:19 -0500

    possibly fixed code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by dvw25 on 11/30/17.
+ */
+
+public class AutoDrive {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Blue.java
@@ -22,6 +22,10 @@ public class Color_Blue extends OpMode {
     private ColorSensor color;
     private DcMotor rf, lf, rb, lb;
 
+    private boolean sawBlue = false;
+    private boolean sawRed = false;
+    private long time = 0;
+
     @Override
     public synchronized void init() {
         colorKnocker = hardwareMap.servo.get("s1");
@@ -42,7 +46,9 @@ public class Color_Blue extends OpMode {
         lb.setPower(a1);
         rf.setPower(a2);
         rb.setPower(a3);
-
+    }
+    private void setMotor(double left, double right) {
+        setMotor(left, right, left, right);
     }
 
 
@@ -56,24 +62,31 @@ public class Color_Blue extends OpMode {
         // If it senses the blue ball then it needs to go backwards to knock off the red ball
         //the reverse if it senses a non blue ball (red)
 
-        if (color.blue() > 90 ){
-            long t = System.currentTimeMillis();
-            long end = t + 2000;
-            while (System.currentTimeMillis() < end) {
-                setMotor(1, 1, 1, 1);
-            }
-
-        } else {
-            long t = System.currentTimeMillis();
-            long end = t + 2000;
-            while (System.currentTimeMillis() < end) {
-                setMotor(-1, -1, -1, -1);
-            }
-
-
+        if (time == 0) {
+            colorKnocker.setPosition(0);
+            //sets position down
         }
 
-        colorKnocker.setPosition(0);
+        if (color.blue() > 90 && !sawBlue && time == 0){
+            time = System.currentTimeMillis();
+            sawBlue = true;
+            setMotor(1, 1);
+        } else if(sawBlue && System.currentTimeMillis() >= time + 2000) {
+            setMotor(0, 0);
+            sawBlue = false;
+            colorKnocker.setPosition(1);
+            //sets position up
+        }
+
+        if (color.red() > 90 && !sawRed && time == 0){
+            time = System.currentTimeMillis();
+            sawRed = true;
+            setMotor(-1, -1);
+        } else if(sawRed && System.currentTimeMillis() >= time + 2000) {
+            setMotor(0, 0);
+            sawRed = false;
+            colorKnocker.setPosition(1);
+        }
     }
     public void stop() {
 
