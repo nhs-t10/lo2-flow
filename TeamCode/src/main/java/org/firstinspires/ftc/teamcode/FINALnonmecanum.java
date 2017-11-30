89493060c5062789806cb354b18e5c095e458eee
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2017-11-30 15:16:59 -0500

    movement with new motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FINALnonmecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FINALnonmecanum.java
@@ -39,7 +39,7 @@ public class FINALnonmecanum extends OpMode {
 
         if (forward>0.25){
 
-            lf.setPower(-forward);
+            lf.setPower(forward);
             rf.setPower(-forward);
             rb.setPower(-forward);
             lb.setPower(forward);
@@ -48,7 +48,7 @@ public class FINALnonmecanum extends OpMode {
 
         if (forward<-0.25){
 
-            lf.setPower(-forward);
+            lf.setPower(forward);
             rf.setPower(-forward);
             rb.setPower(-forward);
             lb.setPower(forward);
@@ -59,7 +59,7 @@ public class FINALnonmecanum extends OpMode {
         if (turning>0.25){
 
             lb.setPower(-turning);
-            lf.setPower(turning);
+            lf.setPower(-turning);
             rf.setPower(-turning);
             rb.setPower(-turning);
 
@@ -68,7 +68,7 @@ public class FINALnonmecanum extends OpMode {
         if (turning<-0.25) {
 
             lb.setPower(-turning);
-            lf.setPower(turning);
+            lf.setPower(-turning);
             rf.setPower(-turning);
             rb.setPower(-turning);
         }
@@ -94,8 +94,8 @@ public class FINALnonmecanum extends OpMode {
     }
 
     double scaleInput(double dVal)  {
-        double[] scaleArray = { 0.0, 0.005, 0.009, 0.01, 0.02, 0.03, 0.07, 0.1,
-                0.230, 0.336, 0.443, 0.550, 0.660, 0.772, 0.85, 1.00, 1.00 };
+        double[] scaleArray = { 0.0, 0.001, 0.005, 0.05, 0.075, 0.1, 0.15, 0.,
+                0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6 };
 
         // get the corresponding index for the scaleInput array.
         int index = (int) (dVal * 16.0);
