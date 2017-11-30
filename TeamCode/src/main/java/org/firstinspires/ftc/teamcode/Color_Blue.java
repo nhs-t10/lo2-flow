b77700a02e34be74b85e422cc6603db53d045449
Lancey Pancey <zlancelot117@gmail.com>
2017-11-30 15:11:08 -0500

    should be ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Blue.java
@@ -4,22 +4,18 @@ package org.firstinspires.ftc.teamcode;
  * Created by dvw24 on 10/10/17.
  */
 
-
-
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 //import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 //import com.qualcomm.robotcore.hardware.NormalizedRGBA;
-//
 //import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
 
 @Autonomous(name = "JewelBlue")
-public class Color_Blue extends LinearOpMode {
+public class Color_Blue extends OpMode {
 
 
     private Servo colorKnocker;
@@ -27,20 +23,7 @@ public class Color_Blue extends LinearOpMode {
     private DcMotor rf, lf, rb, lb;
 
     @Override
-    public synchronized void waitForStart() {
-        super.waitForStart();
-    }
-
-    private void setMotor(double a0, double a1, double a2, double a3) {
-        lf.setPower(a0);
-        lb.setPower(a1);
-        rf.setPower(a2);
-        rb.setPower(a3);
-
-    }
-
-
-    public void runOpMode() {
+    public synchronized void init() {
         colorKnocker = hardwareMap.servo.get("s1");
         color = hardwareMap.colorSensor.get("color");
         rf = hardwareMap.dcMotor.get("m1");
@@ -52,15 +35,28 @@ public class Color_Blue extends LinearOpMode {
 
         color.enableLed(true);
         colorKnocker.setPosition(1);
+    }
+
+    private void setMotor(double a0, double a1, double a2, double a3) {
+        lf.setPower(a0);
+        lb.setPower(a1);
+        rf.setPower(a2);
+        rb.setPower(a3);
+
+    }
+
+
+    public void loop() {
+
         //I have no real clue if this will set the arm correctly but if it does no need to fix it
 
-        // In this code we are on the blue team.
+        //In this code we are on the blue team.
         //This means we need to knock down the red ball.
         //The sensor will be on the right of the arm
         // If it senses the blue ball then it needs to go backwards to knock off the red ball
         //the reverse if it senses a non blue ball (red)
 
-        if (color.blue() > 90) {
+        if (color.blue() > 90 ){
             long t = System.currentTimeMillis();
             long end = t + 2000;
             while (System.currentTimeMillis() < end) {
@@ -78,6 +74,9 @@ public class Color_Blue extends LinearOpMode {
         }
 
         colorKnocker.setPosition(0);
+    }
+    public void stop() {
+
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Red.java
@@ -4,11 +4,8 @@ package org.firstinspires.ftc.teamcode;
  * Created by dvw24 on 10/10/17.
  */
 
-
-
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -18,7 +15,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 //import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
 
 @Autonomous(name = "JewelRed")
-public class Color_Red extends LinearOpMode {
+public class Color_Red extends OpMode {
 
 
     private Servo colorKnocker;
@@ -26,20 +23,7 @@ public class Color_Red extends LinearOpMode {
     private DcMotor rf, lf, rb, lb;
 
     @Override
-    public synchronized void waitForStart() {
-        super.waitForStart();
-    }
-
-    private void setMotor(double a0, double a1, double a2, double a3) {
-        lf.setPower(a0);
-        lb.setPower(a1);
-        rf.setPower(a2);
-        rb.setPower(a3);
-
-    }
-
-
-    public void runOpMode() {
+    public synchronized void init() {
         colorKnocker = hardwareMap.servo.get("s1");
         color = hardwareMap.colorSensor.get("color");
         rf = hardwareMap.dcMotor.get("m1");
@@ -51,6 +35,19 @@ public class Color_Red extends LinearOpMode {
 
         color.enableLed(true);
         colorKnocker.setPosition(1);
+    }
+
+    private void setMotor(double a0, double a1, double a2, double a3) {
+        lf.setPower(a0);
+        lb.setPower(a1);
+        rf.setPower(a2);
+        rb.setPower(a3);
+
+    }
+
+
+    public void loop() {
+
         //I have no real clue if this will set the arm correctly but if it does no need to fix it
 
         //In this code we are on the blue team.
@@ -77,6 +74,9 @@ public class Color_Red extends LinearOpMode {
         }
 
         colorKnocker.setPosition(0);
+    }
+    public void stop() {
+
     }
 }
 
