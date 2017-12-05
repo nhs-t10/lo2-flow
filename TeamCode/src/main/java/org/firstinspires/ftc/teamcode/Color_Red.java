6ec3fa35f03cb3191f9d87f84e14d54ae7d3c1ed
alko08 <amk211@students.needham.k12.ma.us>
2017-12-05 18:33:28 -0500

    fixed Color_Red to be like Color_Blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Red.java
@@ -25,21 +25,12 @@ public class Color_Red extends LinearOpMode {
     private ColorSensor color;
     private DcMotor rf, lf, rb, lb;
 
+    private boolean sawBlue = false;
+    private boolean sawRed = false;
+    private long time = 0;
+
     @Override
-    public synchronized void waitForStart() {
-        super.waitForStart();
-    }
-
-    private void setMotor(double a0, double a1, double a2, double a3) {
-        lf.setPower(a0);
-        lb.setPower(a1);
-        rf.setPower(a2);
-        rb.setPower(a3);
-
-    }
-
-
-    public void runOpMode() {
+    public synchronized void init() {
         colorKnocker = hardwareMap.servo.get("s1");
         color = hardwareMap.colorSensor.get("color");
         rf = hardwareMap.dcMotor.get("m1");
@@ -51,32 +42,57 @@ public class Color_Red extends LinearOpMode {
 
         color.enableLed(true);
         colorKnocker.setPosition(1);
+    }
+
+    private void setMotor(double a0, double a1, double a2, double a3) {
+        lf.setPower(a0);
+        lb.setPower(a1);
+        rf.setPower(a2);
+        rb.setPower(a3);
+    }
+    private void setMotor(double left, double right) {
+        setMotor(left, right, left, right);
+    }
+
+
+    public void loop() {
+
         //I have no real clue if this will set the arm correctly but if it does no need to fix it
 
-        //In this code we are on the blue team.
-        //This means we need to knock down the red ball.
-        //The sensor will be on the right of the arm
-        // If it senses the blue ball then it needs to go backwards to knock off the red ball
-        //the reverse if it senses a non blue ball (red)
+        //In this code we are on the red team.
+        //This means we need to knock down the blue ball.
+        //The sensor will be on the ?right? of the arm (if left reverse below)
+        // If it senses the red ball then it needs to go backwards to knock off the blue ball
+        //the reverse if it senses a non red ball (blue)
 
-        if (color.red() > 90 ){
-            long t = System.currentTimeMillis();
-            long end = t + 2000;
-            while (System.currentTimeMillis() < end) {
-                setMotor(1, 1, 1, 1);
-            }
-
-        } else {
-            long t = System.currentTimeMillis();
-            long end = t + 2000;
-            while (System.currentTimeMillis() < end) {
-                setMotor(-1, -1, -1, -1);
-            }
-
-
+        if (time == 0) {
+            colorKnocker.setPosition(0);
+            //sets position down
         }
 
-        colorKnocker.setPosition(0);
+        if (color.red() > 90 && !sawRed && time == 0){
+            time = System.currentTimeMillis();
+            sawRed = true;
+            setMotor(1, 1);
+        } else if(sawRed && System.currentTimeMillis() >= time + 2000) {
+            setMotor(0, 0);
+            sawRed = false;
+            colorKnocker.setPosition(1);
+            //sets position up
+        }
+
+        if (color.blue() > 90 && !sawBlue && time == 0){
+            time = System.currentTimeMillis();
+            sawBlue = true;
+            setMotor(-1, -1);
+        } else if(sawBlue && System.currentTimeMillis() >= time + 2000) {
+            setMotor(0, 0);
+            sawBlue = false;
+            colorKnocker.setPosition(1);
+        }
+    }
+    public void stop() {
+
     }
 }
 
