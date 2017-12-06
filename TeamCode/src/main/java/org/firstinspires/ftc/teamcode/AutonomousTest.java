af7bbdcbcea3eea4a6549bf65cb97cc12c8d083b
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-12-05 21:05:16 -0500

    This is autonomous movement (feat. the me shaking my head "kenobingly")

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -1,18 +1,12 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorController;
-//import com.qualcomm.robotcore.hardware.Servo;
-//import com.qualcomm.robotcore.hardware.DcMotorSimple;
-//import com.qualcomm.robotcore.util.Range;
-import java.lang.RuntimeException;
-import java.lang.InterruptedException;
-@Autonomous(name = "ZipZapZoop!")
 
+@Autonomous(name = "ZipZapZoop!")
+@SuppressWarnings("unused")
 public class AutonomousTest extends LinearOpMode  {
-    private AutonomousTest  heck = new AutonomousTest();
     private DcMotor lf, lb, rf, rb;
 
     private void wheelSet(double a0, double a1, double a2, double a3) {
@@ -22,13 +16,8 @@ public class AutonomousTest extends LinearOpMode  {
         rb.setPower(a3);
     }
 
-    @Override public synchronized void waitForStart(){
-        super.waitForStart();
-    }
-    public static void Main(){
-    }
-
-     public void runOpMode() /*throws InterruptedException*/ {
+    @Override
+    public void runOpMode() {
         try {
             lf = hardwareMap.dcMotor.get("m0");
             lb = hardwareMap.dcMotor.get("m1");
@@ -38,32 +27,31 @@ public class AutonomousTest extends LinearOpMode  {
             //lb.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
             //rf.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
             //rb.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-            //long timeSpent = System.currentTimeMillis();
 
             lf.setDirection(DcMotor.Direction.REVERSE);
             lb.setDirection(DcMotor.Direction.REVERSE);
             waitForStart();
-           // sleep(5000);
-            //wheelSet(1, 1, -1, -1);
-            //sleep(1000);
-            //wheelSet(0, 0, 0, 0);
-            //sleep(4000);
-            //wheelSet(1, 1, -1, -1);
-            //sleep(2000);
-            //wheelSet(0, 0, 0, 0);
-            //sleep(4000);
-            //wheelSet(1, 1, -1, -1);
-            //sleep(3000);
-            //wheelSet(0, 0, 0, 0);
-            //sleep(4000);
-            //wheelSet(1, 1, -1, -1);
-            //sleep(4000);
-            //wheelSet(0, 0, 0, 0);
+            sleep(5000);
+            wheelSet(1, 1, -1, -1);
+            sleep(1000);
+            wheelSet(0, 0, 0, 0);
+            sleep(4000);
+            wheelSet(1, 1, -1, -1);
+            sleep(2000);
+            wheelSet(0, 0, 0, 0);
+            sleep(4000);
+            wheelSet(1, 1, -1, -1);
+            sleep(3000);
+            wheelSet(0, 0, 0, 0);
+            sleep(4000);
+            wheelSet(1, 1, -1, -1);
+            sleep(4000);
+            wheelSet(0, 0, 0, 0);
         }
-        catch(RuntimeException heck){
-            System.out.println("heck");
-            telemetry.addData("message1","heck we suck");
-
+        catch(Exception e){
+            System.err.println("Error handler:");
+            e.printStackTrace();
+            telemetry.addData("!!!ERROR!!!","We have a problem " + e.getMessage());
         }
 
      }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenuisMachine.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "GenuisMachine")
+public class GenuisMachine {
+
+}
