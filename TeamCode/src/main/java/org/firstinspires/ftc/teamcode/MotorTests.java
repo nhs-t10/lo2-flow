199d8fa72175a860df33afe477a8cd63cf6594a7
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-12-10 11:54:28 -0500

    a private motor test for me

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTests.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@TeleOp(name = "MotorTest")
+@SuppressWarnings("unused")
+public class MotorTests extends OpMode {
+    public static final double POWER = .25;
+    private DcMotor leftMotor, rightMotor;
+
+    @Override
+    public void init(){
+        leftMotor = hardwareMap.dcMotor.get("motor0");
+        rightMotor = hardwareMap.dcMotor.get("motor1");
+    }
+
+    @Override
+    public void loop(){
+        double x = gamepad1.left_stick_x;
+        double y = gamepad1.left_stick_y;
+        if(x>-.25 && x<.25){
+            leftMotor.setPower(y);
+            rightMotor.setPower(y);
+        }
+        if(x>.25){
+            leftMotor.setPower(-y);
+            rightMotor.setPower(y);
+        }
+        if(x<-.25){
+            leftMotor.setPower(y);
+            rightMotor.setPower(-y);
+        }
+        /*if (gamepad1.left_stick_y>.3){
+            leftMotor.setPower(POWER);
+            rightMotor.setPower(POWER);
+
+        }
+        if (gamepad1.left_stick_y<-.3){
+            leftMotor.setPower(-POWER);
+            rightMotor.setPower(-POWER);
+
+        }
+        if(gamepad1.left_stick_x>.3){
+            leftMotor.setPower(POWER);
+            rightMotor.setPower(-POWER);
+        }
+        if(gamepad1.left_stick_x<-.3){
+            leftMotor.setPower(-POWER);
+            rightMotor.setPower(POWER);
+        }*/
+        if(gamepad1.right_trigger>.1){
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+        }
+
+    }
+
+
+
+}
