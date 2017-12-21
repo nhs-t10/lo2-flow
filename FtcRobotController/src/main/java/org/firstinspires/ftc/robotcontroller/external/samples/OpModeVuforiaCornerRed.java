1b81dd271e85848f7d2d680edf37bcd804b64d3b
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2017-12-21 15:18:54 -0500

    spacing

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/OpModeVuforiaCornerRed.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/OpModeVuforiaCornerRed.java
@@ -14,6 +14,7 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 //import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 //import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 //import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
