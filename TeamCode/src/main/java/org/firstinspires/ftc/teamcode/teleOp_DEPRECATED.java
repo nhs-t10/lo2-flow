9347d56b96f5b1f218fac540039337b7fd57086b
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2018-01-16 14:42:51 -0500

    NewTeleOp is the future

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp_DEPRECATED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp_DEPRECATED.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.util.Range;
 /*
  * Created by tripszewczak on 10/5/17.
  */
-@TeleOp(name = "teleop_DEPRECATED")
+//@TeleOp(name = "teleop_DEPRECATED")
 @SuppressWarnings("all")
 public class teleOp_DEPRECATED extends OpMode {
     private DcMotor lf, rf, lb, rb;
