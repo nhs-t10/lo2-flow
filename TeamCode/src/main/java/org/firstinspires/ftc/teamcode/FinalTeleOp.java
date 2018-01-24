86b863da7d0ac4945d421b70ad1b8b2a1d2f2427
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2018-01-24 15:09:18 -0500

    davis did some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
@@ -8,22 +8,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "FinalTeleOp")
-@SuppressWarnings("all")
 public class FinalTeleOp extends AbstractSuper
 {
+    double linear = gamepad1.left_stick_y;
+    double rotation = gamepad1.left_stick_x;
+    double side = gamepad1.right_stick_x;
+    boolean up = gamepad1.right_bumper;
+    boolean down = gamepad1.left_bumper;
+    double top = gamepad1.right_trigger;
+    double bottom = gamepad1.left_trigger;
     public void prepare()
     {
 
     }
     public void loop()
         {
-        double linear = gamepad1.left_stick_y;
-        double rotation = gamepad1.left_stick_x;
-        double side = gamepad1.right_stick_x;
-        boolean up = gamepad1.right_bumper;
-        boolean down = gamepad1.left_bumper;
-        double top = gamepad1.right_trigger;
-        double bottom = gamepad1.left_trigger;
         top = Range.clip(top, 0, 0.5);
         bottom = Range.clip(bottom, 0, 0.5);
 
@@ -31,49 +30,20 @@ public class FinalTeleOp extends AbstractSuper
         {
             drive(linear - rotation, linear + rotation);
         }
-        else
-        {
-            side(side);
-        }
+        else { side(side); }
 
+        if (up) { lift(1); }
 
+        if (down) { lift(-1); }
 
-        if (up)
-        {
-            lift(1);
-        }
+        if ((down == false) && (up == false)) {lift(0);}
 
-        if (down)
-        {
-            lift(-1);
-        }
-
-        if ((down = false) && (up = false))
-        {
-            lift(0);
-        }
-
-        if (top >= 0 || bottom >= 0)
-        {
-            clamp(top, bottom);
-        }
+        if (top >= 0 || bottom >= 0) {clamp(top, bottom);}
 /*for touch sensor, remenber to include if (sensor)
               {
               setPower(0);
 */
-
     }
-
-
-
-
-
-
-
-     public void stop()
-     {
-
-
-     }
+     public void stop() {}
 }
 
