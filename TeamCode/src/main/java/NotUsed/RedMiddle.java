5c0739be6ab675c4ad7da161697696486fad0fb5
alko08 <amk211@students.needham.k12.ma.us>
2018-01-30 20:01:33 -0500

    possible auto code to test later

--- TeamCode/src/main/java/NotUsed/RedMiddle.java
+++ TeamCode/src/main/java/NotUsed/RedMiddle.java
@@ -12,17 +12,17 @@ import org.firstinspires.ftc.teamcode.AbstractSuper;
 @SuppressWarnings("unused")
 public class RedMiddle extends AbstractSuper
 {
+    int time = 0;
     public void prepare()
     {
 
     }
     public void loop()
     {
-        arm(0.6);
+        /*arm(0.6);
         //the next piece of code knocks off the blue ball (must be on red team middle)
         if (color.red() > 20)
         {
-            clamp(0,0.13);
             driveFor(700, 1, 1);
             arm(0);
             sideFor(100,-1);
@@ -36,7 +36,36 @@ public class RedMiddle extends AbstractSuper
             sideFor(100, -1);
             driveFor(400,0, 0);
             stop();
+        }*/
+
+        if (time > 85)
+        {
+            drive(0, 0);
+            clamp(0, 0);
+            lift(0);
         }
+            else
+            {
+                if (time > 80 && time < 85) {
+                    drive(-1, -1);
+                    clamp(0,0);
+                    lift(0);
+                }
+                else
+                {
+                    if (time <5) {clamp(0,0.13);}
+                    else
+                    {
+                        drive(1, 1);
+                        clamp(0,0.13);
+                        lift(0.15);
+                    }
+                }
+            }
+
+
+        time ++;
+
     }
     public void stop()
     {
