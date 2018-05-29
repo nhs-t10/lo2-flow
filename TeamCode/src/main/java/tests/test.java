82badb3ff8f169d6ee613ddc43aac0ef91d875a0
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2018-05-29 19:20:47 -0400

    lel
    it's called test

---
+++ TeamCode/src/main/java/tests/test.java
@@ -0,0 +1,94 @@
+package tests;
+
+/*
+ Created by tripszewczak on 1/12/18.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.AbstractSuper;
+
+@SuppressWarnings("all")
+@TeleOp(name = "FinalTeleOp")
+public class test extends testsuper
+{
+
+    public void prepare()
+    {
+    }
+    public void loop() {
+        //double linear = gamepad1.left_stick_y;
+        //double rotation = gamepad1.left_stick_x;
+        //double side = gamepad1.right_stick_x;
+        //boolean extend = gamepad1.right_bumper;
+        //boolean down = gamepad1.left_bumper;
+        /*double top = gamepad1.left_trigger;
+        double bottom = gamepad1.right_trigger;*/
+
+        double in = gamepad1.left_trigger;
+        double out = gamepad1.right_trigger;
+
+        boolean leftspin = gamepad1.dpad_left;
+        boolean rightspin = gamepad1.dpad_right;
+        double ls = 0;
+        double rs = 0;
+        //boolean B = gamepad1.b;
+
+
+        if (out > 0)
+        {
+            extend(out, 0);
+        }
+        if (in > 0)
+        {
+            extend(0, in);
+        }
+
+        if (leftspin)
+        {
+            ls = 0.6;
+        }
+        if (rightspin)
+        {
+            rs = -0.6;
+        }
+
+
+        /*top = Range.clip(top, 0, 0.13);
+        //bottom = Range.clip(bottom, 0, 0.13);
+
+        if (side == 0) {
+            drive(.7*(linear - rotation), .7*(linear + rotation));
+        } else {
+            side(.7*side);
+        }
+
+        if (up)
+        {
+            lift(0.15);
+        }
+
+        if (B)
+        {
+            lift(0.35);
+        }
+
+        if (down)
+        {
+            lift(-0.1);
+        }
+
+        //if ((down == false) && (up == false)) {lift(0);}
+
+
+        if (top >= 0 || bottom >= 0) {clamp(top, bottom);}
+
+/*for touch sensor, remenber to include if (sensor)
+              {
+              setPower(0);
+*/
+    }
+     public void stop() {}
+}
+
---
+++ TeamCode/src/main/java/tests/testsuper.java
@@ -0,0 +1,94 @@
+package tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.Timer;
+
+
+@SuppressWarnings("all")
+public abstract class testsuper extends OpMode {
+    public DcMotor extend, spin;
+    public Servo l, r, colorArm, b2, b1, t1, t2, a0, l0;
+    public ColorSensor color;
+    //private long time = System.currentTimeMillis();
+    public double forwardA = -1;
+    public double backA = 1;
+    public double leftA = -1;
+    public double rightA = 1;
+    private Timer timer;
+    private double interval;
+    double time = 0;
+
+
+    @Override
+    public void init() {
+        spin = hardwareMap.dcMotor.get("m0");
+        extend = hardwareMap.dcMotor.get("m1");
+        /*l0 = hardwareMap.servo.get("l0");
+        t1 = hardwareMap.servo.get("c0");
+        t2 = hardwareMap.servo.get("c1");
+        b1 = hardwareMap.servo.get("c2");
+        b2 = hardwareMap.servo.get("c3");
+        a0 = hardwareMap.servo.get("c4");
+
+        color = hardwareMap.colorSensor.get("color");
+
+        b1.setDirection(Servo.Direction.REVERSE);
+        b2.setDirection(Servo.Direction.REVERSE);
+        t1.setDirection(Servo.Direction.REVERSE);
+        t2.setDirection(Servo.Direction.REVERSE);
+        a0.setDirection((Servo.Direction.REVERSE));
+        l0.setPosition(0);
+        a0.setPosition(0);
+        color.enableLed(true);*/
+        prepare();
+
+
+    }
+
+    public abstract void prepare();
+
+    /*public void drive(double left, double right) {
+        lf.setPower(left);
+        lb.setPower(left);
+        rf.setPower(right);
+        rb.setPower(right);
+
+    }
+
+    private final double setInterval() {
+        if (interval <= 1.0)
+            timer.cancel();
+        return --interval;
+        //telemetry.addData("Interval: " , String.valueOf(interval));
+    }
+
+*/
+    public void extend(double right, double left)
+    {
+        extend.setPower(right);
+        extend.setPower(-left);
+    }
+
+    public void spin(double ls, double rs)
+    {
+        spin.setPower(ls); spin.setPower(rs);
+    }
+
+    public void clamp(double top, double bottom)
+    {
+        t1.setPosition(Math.abs(top - 1));
+        t2.setPosition(top);
+        b1.setPosition(Math.abs(bottom - 1));
+        b2.setPosition(bottom);
+    }
+
+
+    /*public void lift(double positon) {
+        l0.setPosition(positon);
+    }*/
+}
